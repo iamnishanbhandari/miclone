f8e01ff2e99cfd228c3ad6b9a0f9abdccd9b6e8b
Nishan Bhandari <bnishan18@gmail.com>
2022-05-02 00:18:59 -0700

    hello

---
+++ miclone/src/components/slider/Slider.js
@@ -0,0 +1,22 @@
+import Carousel from 'react-bootstrap/Carousel'
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Slider = ({ start }) => {
+  return (
+    <>
+      <Carousel fade>
+        {start.map((item) => ( 
+          <Carousel.Item>
+            <img className='d-block w-100'
+              src={item}
+              alt='first slide'
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </>
+  )
+}
+
+export default Slider
